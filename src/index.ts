10e23d3a8aaaaf468675a841a3de06e90e3a7fe5
Yuhi Ishikura <yuhi.ishikura@gmail.com>
2021-03-06 17:40:59 +0900

    Send settings with message.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import joplin from 'api';
 import { ContentScriptType } from 'api/types';
+import { registerSettings, loadSettings } from './settings'
 
 const contentScriptId = 'plantuml';
 
 joplin.plugins.register({
 	onStart: async function () {
+		await registerSettings();
+		joplin.contentScripts.onMessage(contentScriptId, async (message) => {
+			if ("settings" === message) {
+				return loadSettings();
+			}
+			return null;
+		});
 		await joplin.contentScripts.register(
 			ContentScriptType.MarkdownItPlugin,
 			contentScriptId,
--- src/markdownItPlantUmlPlugin.ts
+++ src/markdownItPlantUmlPlugin.ts
@@ -1,6 +1,9 @@
 export default function (context) {
     return {
         plugin: function (markdownIt, options) {
+            context.postMessage('settings').then(settings => {
+                console.log(settings);
+            });
             const markdownItPlantUml = require('markdown-it-plantuml');
             markdownIt.use(markdownItPlantUml, {
                 // How can I refer settings from here?
